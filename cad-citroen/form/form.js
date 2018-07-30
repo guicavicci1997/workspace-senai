60a56ba2c4baffe777d1afb8af1cd10aa3e7a6d6
gneves <gneves@mmcafe.com.br>
2018-07-30 17:27:58 -0300

    validacao form

--- cad-citroen/form/form.js
+++ cad-citroen/form/form.js
@@ -1,12 +1,13 @@
 function validaForm(){
-    nome = document.getElementsByName("nome")
-
-    if (nome != null) {
-        return true;
-    }
-    else {
+    if (document.form.nome.value==""){
         alert("O nome não pode estár em branco");
+        document.forms.nome.focus();
+        return false;
 
+    }
+
+    else{
+        return true;
     }
 
 }

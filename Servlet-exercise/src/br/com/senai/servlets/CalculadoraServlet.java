77aa2c4c0aea7db63df41cf01f0d28a5ee6d6860
unknown <jweb180723@LAB11-WS10.info.local>
2018-07-31 19:41:07 -0300

    projeto servlet

--- Servlet-exercise/src/br/com/senai/servlets/CalculadoraServlet.java
+++ Servlet-exercise/src/br/com/senai/servlets/CalculadoraServlet.java
@@ -4,8 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-public class CalculadoraServlet extends HttpServlet{
+public class CalculadoraServlet extends HttpServlet {
 
 	/**
 	 * 
@@ -13,11 +12,36 @@ public class CalculadoraServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
 	public void service(HttpServletRequest req, HttpServletResponse rep) {
-		
+
 		Integer n1 = Integer.parseInt(req.getParameter("n1"));
 		Integer n2 = Integer.parseInt(req.getParameter("n2"));
-		req.getParameter("operacao");
-		
+		String op = req.getParameter("operacao");
+		Integer result;
+
+		switch (op) {
+
+		case "soma":
+			result = n1 + n2;
+			System.out.println(result);
+			break;
+
+		case "subtração":
+			result = n1 - n2;
+			System.out.println(result);
+			break;
+
+		case "divisão":
+			result = n1 / n2;
+			System.out.println(result);
+			break;
+
+		case "multiplicacao":
+			result = n1 * n2;
+			System.out.println(result);
+			break;
+
+		}
+
 	}
-	
+
 }

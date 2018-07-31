399ba967fbe641d1b2855923c956b0ddb46e96da
unknown <jweb180723@LAB11-WS10.info.local>
2018-07-31 20:18:44 -0300

    fix null pointer

--- Servlet-exercise/src/br/com/senai/servlets/CalculadoraServlet.java
+++ Servlet-exercise/src/br/com/senai/servlets/CalculadoraServlet.java
@@ -1,5 +1,7 @@
 package br.com.senai.servlets;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,35 +13,57 @@ public class CalculadoraServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public void service(HttpServletRequest req, HttpServletResponse rep) {
+	public void service(HttpServletRequest req, HttpServletResponse resp) {
 
-		Integer n1 = Integer.parseInt(req.getParameter("n1"));
-		Integer n2 = Integer.parseInt(req.getParameter("n2"));
 		String op = req.getParameter("operacao");
 		Integer result;
+		
+		Integer n1 = Integer.parseInt(req.getParameter("n1"));
+		if (n1.equals("")) {
+			op = "erro";
+			
+		}
+		
+		Integer n2 = Integer.parseInt(req.getParameter("n2"));
+		if(n2.equals("")) {
+			op = "erro";
+			
+		}
+		try {
 
-		switch (op) {
+			switch (op) {
 
-		case "soma":
-			result = n1 + n2;
-			System.out.println(result);
-			break;
+			case "soma":
+				result = n1 + n2;
+				System.out.println(result);
+				resp.getWriter().println("Resultado: " + result);
+				break;
 
-		case "subtração":
-			result = n1 - n2;
-			System.out.println(result);
-			break;
+			case "sub":
+				result = n1 - n2;
+				System.out.println(result);
+				resp.getWriter().println("Resultado: " + result);
+				break;
 
-		case "divisão":
-			result = n1 / n2;
-			System.out.println(result);
-			break;
+			case "div":
+				result = n1 / n2;
+				System.out.println(result);
+				resp.getWriter().println("Resultado: " + result);
+				break;
 
-		case "multiplicacao":
-			result = n1 * n2;
-			System.out.println(result);
-			break;
+			case "mult":
+				result = n1 * n2;
+				System.out.println(result);
+				resp.getWriter().println("Resultado: " + result);
+				break;
+			default:
+				System.out.println("Digite uma opção valida");
+				break;
 
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}

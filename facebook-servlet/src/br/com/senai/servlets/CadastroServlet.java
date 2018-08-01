5470a6161404ee7d06b448faa3ca60e25e639680
gneves <gneves@mmcafe.com.br>
2018-08-01 17:14:02 -0300

    servlet cadastro

---
+++ facebook-servlet/src/br/com/senai/servlets/CadastroServlet.java
@@ -0,0 +1,69 @@
+package br.com.senai.servlets;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.senai.database.Database;
+import br.com.senai.models.Usuario;
+import br.com.senai.utils.FilesUtils;
+
+/**
+ * Servlet implementation class CadastroServlet
+ */
+@WebServlet("/cadastro.html")
+public class CadastroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CadastroServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Path caminho = Paths.get(FilesUtils.webInfDir.toString(), "cadastro.html");
+		System.out.println(caminho);
+    	
+		String html = new String(Files.readAllBytes(caminho.toAbsolutePath()));
+		System.out.println(html);
+		
+		response.getWriter().println(html);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Usuario usuario = new Usuario();
+		usuario.setNome(request.getParameter("nome"));
+		usuario.setSobrenome(request.getParameter("sobrenome"));
+		usuario.setEmail(request.getParameter("email"));
+		usuario.setTelefone(request.getParameter("telefone"));
+		usuario.setSenha(request.getParameter("senha"));
+		
+		//Database.USUARIOS.put("a@a", newUsuario(null,null,"a@a", null, "123", null, null))
+		
+		Database.USUARIOS.put(usuario.getEmail(), usuario);
+		System.out.println("a");
+		System.out.println("a");
+		System.out.println("a");
+		System.out.println(usuario);
+		
+		
+	}
+
+}

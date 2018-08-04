ddae1551f6846c0f13e67438944f9676ff77a9d3
unknown <jweb180723@LAB11-WS10.info.local>
2018-08-03 22:00:11 -0300

    implementacao controler index

---
+++ facebug-thymeleaf/src/main/java/br/com/senai/facebugthymeleaf/FacebugThymeleafApplication.java
@@ -0,0 +1,12 @@
+package br.com.senai.facebugthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebugThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebugThymeleafApplication.class, args);
+	}
+}
---
+++ facebug-thymeleaf/src/main/java/br/com/senai/facebugthymeleaf/IndexController.java
@@ -0,0 +1,23 @@
+package br.com.senai.facebugthymeleaf;
+
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping
+public class IndexController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String main (HttpServletRequest request) {
+		
+		request.setAttribute("datetime", LocalDateTime.now());
+		return "index";
+			
+				
+	}
+}
